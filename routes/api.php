6d64ca201eefb153d1a1d41261f98649343a3e12
fernandooliveiralima <oliveirafernando.contato@gmail.com>
2024-07-04 18:57:31 -0300

    sistema de autenticacao e autorizacao com jwt finalizados

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BrandCarController;
 use App\Http\Controllers\CarController;
 use App\Http\Controllers\CostumerController;
@@ -13,8 +14,22 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('brand', BrandCarController::class);
-Route::apiResource('car', CarController::class);
-Route::apiResource('costumer', CostumerController::class);
-Route::apiResource('location', LocationController::class);
-Route::apiResource('modelCar', ModelCarController::class);
+
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+
+
+
+Route::group(['middleware' => 'auth:api', 'prefix' => 'auth'], function ($router) {
+
+    Route::post('/me', [AuthController::class, 'me']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('/brand', BrandCarController::class);
+    Route::apiResource('/car', CarController::class);
+    Route::apiResource('/costumer', CostumerController::class);
+    Route::apiResource('/location', LocationController::class);
+    Route::apiResource('/modelCar', ModelCarController::class);
+});
